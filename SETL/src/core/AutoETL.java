c81c77ff4240ae6f1198bbd0927e0ffe38c9545a
Amrit Bhattacharjee <ab181094@gmail.com>
2020-02-15 08:31:26 +0600

    Create AutoETL.java

---
+++ SETL/src/core/AutoETL.java
@@ -0,0 +1,19 @@
+package core;
+
+public class AutoETL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String basePath = "";
+		String mapFile = basePath + "";
+		String targetTBoxFile = "";
+		
+		performAutoETL(mapFile, targetTBoxFile);
+	}
+
+	private static void performAutoETL(String mapFile, String targetTBoxFile) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
